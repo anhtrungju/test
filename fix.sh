e46a121d287e0d127df0a7c60d7b1595e40fc860
anhtrungju <73046556+anhtrungju@users.noreply.github.com>
2025-07-03 14:16:48 +0000

    Add configuration files and scripts for server setup and mod management

---
+++ fix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Cài đặt OpenJDK 21
+sudo apt update
+sudo apt install -y openjdk-21-jdk
+
+# Cài đặt Playit
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install -y playit
+
+# Cài đặt Ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+sudo apt update
+sudo apt install -y ngrok
+
+echo "Cài đặt hoàn tất!"
+
